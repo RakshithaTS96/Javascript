3fd40da26a44144fc38027a7aff74f762ca5da0d
RakshithaTS96 <126746779+RakshithaTS96@users.noreply.github.com>
2024-07-23 12:05:26 +0530

    Add files via upload

---
+++ BasicOf(variable, datatypes and operators)/Datatypes.js
@@ -0,0 +1,35 @@
+// Types of datatypes
+
+//1.Number datatype
+
+var n = 100
+console.log(n)
+console.log(typeof(n))
+
+//2. String datatype
+
+let name = 'Rakshitha'
+let message = 'Hello World!!!'
+console.log(name)
+console.log(message)
+console.log(typeof(name))
+
+//3. Boolean datatype
+
+let isAdmin = true;
+let isLoggedIn = false;
+console.log(isAdmin)
+console.log(typeof(isAdmin))
+console.log(isLoggedIn)
+
+//4. Null datatype
+
+let s = null
+console.log(s)
+console.log(typeof(s))
+
+//5. Undefined datatype
+
+let y
+console.log(y)
+console.log(typeof(y))
---
+++ BasicOf(variable, datatypes and operators)/variable.js
@@ -0,0 +1,35 @@
+// Types of variable :
+
+//1. var
+
+var name = 'Rakshitha'
+console.log(name);
+
+if (true) {
+    age = 24;
+}
+console.log(age)
+
+//2. let 
+
+let city = 'Bangalore';
+console.log(city); 
+
+if (true) {
+  let country = 'India';
+  console.log(country); 
+}
+
+// 3. const
+
+const pi = 3.14;
+console.log(pi); 
+
+if (true) {
+  const gravity = 9.8;
+  console.log(gravity); 
+}
+
+
+
+
